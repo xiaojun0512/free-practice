b136b0699e2cefe1dea591f8e094df591e957869
xiaojun0512 <17711397590@163.com>
2022-01-21 14:06:28 +0800

    'init'

---
+++ src/main/java/com/cd/wj/freepractice/FreePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cd.wj.freepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cd/wj/freepractice/FreePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cd.wj.freepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
