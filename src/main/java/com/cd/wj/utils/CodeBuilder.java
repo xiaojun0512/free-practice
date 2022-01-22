610b6b8e8aa38f0dcaa12876d58abd6fb268d3ca
xiaojun0512 <17711397590@163.com>
2022-01-22 15:18:15 +0800

    代码生成器完善

--- src/main/java/com/cd/wj/utils/CodeBuilder.java
+++ src/main/java/com/cd/wj/utils/CodeBuilder.java
@@ -1,9 +1,16 @@
 package com.cd.wj.utils;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 自动生成mybatis plus的相关代码
@@ -13,7 +20,7 @@ public class CodeBuilder {
     //创建者
     private static final String author = "xj";
     //表名,多个表名之间用逗号隔开
-    private static final String tableNames = "payment";
+    private static final String tableNames = "my_test";
 
     //数据库配置
     private static final String dbUrl = "jdbc:mysql://127.0.0.1:3306/mytest?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true";
@@ -34,6 +41,9 @@ public class CodeBuilder {
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setAuthor(author);
         gc.setOpen(false);
+        gc.setServiceName("%sService"); //生成的service接口名字首字母是否为I，这样设置就没有
+//        gc.setFileOverride(true); //第二次生成会把第一次生成的覆盖掉
+        gc.setBaseResultMap(true); //mapper.xml生成resultMap
         //实体属性 Swagger2 注解
         gc.setSwagger2(true);
         mpg.setGlobalConfig(gc);
@@ -55,42 +65,44 @@ public class CodeBuilder {
         pc.setServiceImpl("service.impl");
         mpg.setPackageInfo(pc);
 
+//*********************以下是配置mapper.xml文件到resources目录下******************************
         // 自定义配置
-//        InjectionConfig cfg = new InjectionConfig() {
-//            @Override
-//            public void initMap() {
-//                // to do nothing
-//            }
-//        };
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
 
         // 如果模板引擎是 freemarker
-//        String templatePath = "/templates/mapper.xml.ftl";
+        String templatePath = "/templates/mapper.xml.ftl";
         // 如果模板引擎是 velocity
-        // String templatePath = "/templates/mapper.xml.vm";
+//         String templatePath = "/templates/mapper.xml.vm";
 
         // 自定义输出配置
-//        List<FileOutConfig> focList = new ArrayList<>();
+        List<FileOutConfig> focList = new ArrayList<>();
         // 自定义配置会被优先输出
-//        focList.add(new FileOutConfig(templatePath) {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-//            }
-//        });
-        /*
-        cfg.setFileCreate(new IFileCreate() {
+        focList.add(new FileOutConfig(templatePath) {
             @Override
-            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录");
-                return false;
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + File.separator + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-        */
-//        cfg.setFileOutConfigList(focList);
-//        mpg.setCfg(cfg);
+
+//        cfg.setFileCreate(new IFileCreate() {
+//            @Override
+//            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+//                // 判断自定义文件夹是否需要创建
+//                checkDir("调用默认方法创建的目录");
+//                return false;
+//            }
+//        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+//***************************************************结束
 
         // 配置模板
         TemplateConfig templateConfig = new TemplateConfig();
@@ -101,14 +113,14 @@ public class CodeBuilder {
         // templateConfig.setService();
         // templateConfig.setController();
 
-        templateConfig.setXml(null);
+        templateConfig.setXml(null);    //如果要在/src/main/java/.../mapper/xml下生成mapper.xml文件,请注释此行
         mpg.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//        strategy.setSuperEntityClass("com.baomidou.mybatisplus.extension.activerecord.Model");
+//        strategy.setSuperEntityClass("com.baomidou.mybatisplus.extension.activerecord.Model");    //设置实体类父类
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
 
