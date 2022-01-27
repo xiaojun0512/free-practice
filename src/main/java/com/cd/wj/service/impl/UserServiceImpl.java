c55f799c0453eed970527851b4fa4278bf65166d
xiaojun0512 <17711397590@163.com>
2022-01-27 14:41:16 +0800

    自定义md5生成规则

--- src/main/java/com/cd/wj/service/impl/UserServiceImpl.java
+++ src/main/java/com/cd/wj/service/impl/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -40,6 +39,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     @Override
     public String addUser(User user) {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("name",user.getName());
+        User one = baseMapper.selectOne(wrapper);
+        if (ObjectUtils.isNotEmpty(one)) {
+            throw new RuntimeException("用户已存在");
+        }
         user.setId(UUID.randomUUID().toString());
         user.setCreateTime(LocalDateTime.now());
         user.setPassword(getMd5(user.getName(), user.getPassword()));
@@ -52,11 +57,18 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
     /**
      * 设置md5生成方式为:(姓名+密码)后的字符串生成md5码
+     * 具体生成方法自行调整,越复杂越安全
      * @param name 姓名
      * @param password 密码
      * @return md5
      */
-    public String getMd5(String name,String password) {
-        return MD5Util.getMD5ByBytes((name + password).getBytes(StandardCharsets.UTF_8));
+    private String getMd5(String name,String password) {
+        String md5 = MD5Util.getMD5ByBytes((name + password).getBytes(StandardCharsets.UTF_8));
+        char c = password.charAt(0);
+        char c1 = password.charAt(password.length() - 1);
+        char c2 = password.charAt(password.length() / 3);
+        md5 = c + md5 + c1;
+        md5 = md5.substring(0,md5.length() / 3) + c2 + md5.substring(md5.length() / 3);
+        return MD5Util.getMD5ByBytes(md5.getBytes(StandardCharsets.UTF_8));
     }
 }
